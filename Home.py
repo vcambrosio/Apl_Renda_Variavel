0077c06449159a63ae6343c377837ab4050405b6
Seu Nome <seuemail@example.com>
2024-12-12 16:03:37 -0300

    1

--- Home.py
+++ Home.py
@@ -1,16 +1,23 @@
 import streamlit as st
+
+# Configuração da página
 st.set_page_config(
     page_title="Bem vindo!",
     page_icon="👋",
 )
-# Exibe a logomarca na barra lateral
-st.logo("data\logo1.png", size="large")
+
+# Exibe a logomarca na barra lateral, acima de tudo
+st.sidebar.image("data/logo1.png", width=200)
+
+# Mensagem principal na página
 st.write("# Welcome to Streamlit! 👋")
+
+# Mensagem na barra lateral
 st.sidebar.success("Selecione uma aplicação acima")
 
-
+# Texto adicional
 st.markdown(
     """
-  Nova versão do aplicativo de renda variavel
+  Nova versão do aplicativo de renda variável
 """
 )
