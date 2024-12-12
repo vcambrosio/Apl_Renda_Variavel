d181f2d629331d4e920719b98df11607dfb3f98b
Seu Nome <seuemail@example.com>
2024-12-12 16:11:56 -0300

    az

--- pages/1_Lista_ativos.py
+++ pages/1_Lista_ativos.py
@@ -1,14 +1,9 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Bem vindo!",
-    page_icon="👋",
+    page_title="Ativos",
+    
 )
-
-
-
-# Exibe a logomarca na barra lateral
-st.logo("data\logo1.png", size="large")
 
 
 st.write("# Welcome to Streamlit! 👋")
